8aa6d96d345db8bbee150f9a3ad833c1424b027e
murakom <m.koudai4@gmail.com>
2023-06-01 15:27:29 +0900

    test

---
+++ backend/send_conv.py
@@ -0,0 +1,24 @@
+import requests
+
+url = 'http://127.0.0.1:15000/api/send-message'
+# いくつかのメッセージを送信
+with open('./sample_text/sample.txt', 'r') as f:
+    conversations = f.readlines()
+
+messages = []
+for conv in conversations:
+    if conv == '\n':
+        pass
+    else:
+        name, msg = conv.split(':')
+        messages.append({"message": msg, "userName": name})
+
+print(messages)
+for msg in messages:
+    print(msg)
+    response = requests.post(url, data=msg)
+    print(response)
+    if response.json()['success']:
+        print("メッセージが追加されました。")
+    else:
+        print("メッセージ追加に失敗しました。")
